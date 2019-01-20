d6d2fe183cf6f58538a38507b40fb0bd7aa61874
PuwasalaG <puwasala.gamakumara@monash.edu>
2019-01-21 00:17:19 +1100

    Section 2(Data description) is added

---
+++ Chapter/Figs/Diagram_3D/Schematic-3D.R
@@ -0,0 +1,22 @@
+library(mvtnorm)
+library(copula)
+library(scatterplot3d)
+library(clusterGeneration)
+library(tidyverse)
+
+set.seed(1989)
+
+Sigma <- matrix(c(7, 3.5,
+                  3.5, 5), nrow = 2, ncol = 2, byrow = T)
+
+X <- rmvnorm(50, c(-2, 0), sigma = Sigma) %>% as.data.frame()
+X[,3] <- X[,1] + X[,2]
+
+colnames(X) <- c("X1", "X2", "X3")
+
+s3d <- scatterplot3d(X, type = "h", color = "red",
+                     angle=55, pch = 16, grid=TRUE, box=FALSE)
+
+# Add regression plane
+my.lm <- lm(X$X3 ~ X$X1 + X$X2)
+s3d$plane3d(my.lm, draw_lines = T, draw_polygon = T)
