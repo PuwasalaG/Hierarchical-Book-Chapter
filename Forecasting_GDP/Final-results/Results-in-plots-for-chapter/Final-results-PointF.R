2cb200017b6e9410d36c1a8ca5794052cf3d7640
PuwasalaG <puwasala.gamakumara@monash.edu>
2019-01-22 22:49:31 +1100

    Create plots for Naive Vs Base comparison

--- Forecasting_GDP/Final-results/Results-in-plots-for-chapter/Final-results-PointF.R
+++ Forecasting_GDP/Final-results/Results-in-plots-for-chapter/Final-results-PointF.R
@@ -384,7 +384,7 @@ EXP_Disaggregate_PointF %>% mutate(Scoring_rule = factor(Scoring_rule, levels = 
   ggplot(aes(x = h, y = Score, color = Method, shape = Method)) +
   geom_hline(yintercept = 0, color = "grey") +
   geom_point(size = 3) + 
-  scale_color_manual(values = c("brown", "green3", "Blue", "Purple")) +
+  scale_color_manual(values = c("green3", "Blue", "Purple", "brown")) +
   scale_shape_manual(values = 0:3) +
   facet_wrap(~ `Scoring_rule`, scales = "free_y") +  ylab("Skill score %") + 
   ggtitle("Across all bottom level series") -> EXP_PointF_Disaggregates
@@ -444,3 +444,198 @@ EXP_All.series_PointF %>% mutate(Scoring_rule = factor(Scoring_rule, levels = un
 ggarrange(EXP_PointF_GDPE, EXP_PointF_Aggregates, EXP_PointF_Disaggregates, EXP_PointF_All.series, ncol=2, nrow=2, 
           common.legend = TRUE, legend="bottom")
 
+
+
+
+####Naive vs Base
+
+
+
+rm(list = ls())
+#Income approach
+load("Point-forecasting/INC-PointForecasts-ExpandingW.RData")
+
+Method_Order <- c("Base", "Naive")
+
+#For top level
+
+#MSE
+INC_GDPI_PointF_arima <- Score_arima %>% dplyr::filter(`Series`=="Gdpi") 
+
+INC_GDPI_ARIMA_MSE <- INC_GDPI_PointF_arima %>% dplyr::select(`F-method`, `R-method`, `Forecast Horizon`, `MSE` ) %>% 
+  spread(key = `Forecast Horizon`, value = `MSE`) %>% 
+  mutate(`R-method` = replace(`R-method`, 6, "Naive")) %>%
+  dplyr::select(-`F-method`) %>%  
+  rename("Method" = "R-method") %>%
+  slice(match(Method_Order, `Method`)) %>% as.data.frame()
+
+INC_GDPI_ARIMA_MSE %>% gather(-`Method`, key = "h", value = "MSE") -> INC_GDPI_ARIMA_MSE
+
+
+INC_GDPI_ARIMA_MSE %>% ggplot(aes(x = h, y = MSE, color = Method, shape = Method)) +
+  geom_point(size = 3) + 
+  scale_color_manual(values = c("green3", "brown")) +
+  scale_shape_manual(values=0:1) + ylab("MSE ") + ggtitle("Top level")+ 
+  theme(legend.position="bottom") +
+  theme(plot.title = element_text(size = 10, face = "italic")) -> Plot_INC_PointF_GDPI_MSE
+
+
+#MASE
+INC_GDPI_ARIMA_MASE <- INC_GDPI_PointF_arima %>% dplyr::select(`F-method`, `R-method`, `Forecast Horizon`, `MASE` ) %>% 
+  spread(key = `Forecast Horizon`, value = `MASE`) %>% 
+  mutate(`R-method` = replace(`R-method`, 6, "Naive")) %>%
+  dplyr::select(-`F-method`) %>%  
+  rename("Method" = "R-method") %>%
+  slice(match(Method_Order, `Method`)) %>% as.data.frame()
+
+INC_GDPI_ARIMA_MASE %>% gather(-`Method`, key = "h", value = "MASE") -> INC_GDPI_ARIMA_MASE
+
+INC_GDPI_ARIMA_MASE %>% ggplot(aes(x = h, y = MASE, color = Method, shape = Method)) +
+  geom_point(size = 3) + 
+  scale_color_manual(values = c("green3", "brown")) +
+  scale_shape_manual(values=0:1) + ylab("MASE ") + ggtitle("Top level")  + 
+  theme(legend.position="bottom") +
+  theme(plot.title = element_text(size = 10, face = "italic")) -> Plot_INC_PointF_GDPI_MASE
+
+
+##All levels 
+Score_arima %>% dplyr::select(-`Series`) %>% 
+  group_by(`F-method`, `R-method`, `Forecast Horizon`) %>%
+  summarise(Avg_MSE = round(mean(`MSE`), digits = 2), Avg_MASE = round(mean(`MASE`), digits = 2)) -> INC_Score_all.series_ARIMA
+
+#MSE
+INC_Score_all.series_ARIMA %>% dplyr::select(-`Avg_MASE`) %>% 
+  spread(key = `Forecast Horizon`, value = Avg_MSE) %>% 
+  ungroup() %>% dplyr::select(-`F-method`) %>%  
+  mutate(`R-method` = replace(`R-method`, 6, "Naive")) %>% rename("Method" = "R-method") %>%
+  slice(match(Method_Order, `Method`)) %>% as.data.frame() -> INC_All.series.ARIMA_MSE
+
+INC_All.series.ARIMA_MSE %>% gather(-`Method`, key = "h", value = "MSE") -> INC_All.series.ARIMA_MSE
+
+INC_All.series.ARIMA_MSE %>% ggplot(aes(x = h, y = MSE, color = Method, shape = Method)) +
+  geom_point(size = 3) + 
+  scale_color_manual(values = c("green3", "brown")) +
+  scale_shape_manual(values=0:1) + ylab("MSE ") + ggtitle("All levels") +
+  theme(plot.title = element_text(size = 10, face = "italic")) -> Plot_INC_PointF_All.series_MSE
+
+#MASE
+INC_Score_all.series_ARIMA %>% dplyr::select(-`Avg_MSE`) %>% 
+  spread(key = `Forecast Horizon`, value = Avg_MASE) %>% 
+  ungroup() %>% dplyr::select(-`F-method`) %>%  
+  mutate(`R-method` = replace(`R-method`, 6, "Naive")) %>% rename("Method" = "R-method") %>%
+  slice(match(Method_Order, `Method`)) %>% as.data.frame() -> INC_All.series.ARIMA_MASE
+
+INC_All.series.ARIMA_MASE %>% gather(-`Method`, key = "h", value = "MASE") -> INC_All.series.ARIMA_MASE
+
+INC_All.series.ARIMA_MASE %>% ggplot(aes(x = h, y = MASE, color = Method, shape = Method)) +
+  geom_point(size = 3) + 
+  scale_color_manual(values = c("green3", "brown")) +
+  scale_shape_manual(values=0:1) + ylab("MASE ") + ggtitle("All levels") +
+  theme(plot.title = element_text(size = 10, face = "italic")) -> Plot_INC_PointF_All.series_MASE
+
+
+
+#Expenditure approach
+
+rm(list=ls()[! ls() %in% c("Plot_INC_PointF_GDPI_MSE", "Plot_INC_PointF_GDPI_MASE", "Plot_INC_PointF_All.series_MSE",
+                           "Plot_INC_PointF_All.series_MASE")])
+load("Point-forecasting/EXP-PointForecasts-ExpandingW.RData")
+
+Method_Order <- c("Base", "Naive")
+
+#For top level - Income approach
+
+#MSE
+EXP_GDPE_PointF_arima <- Score_arima %>% dplyr::filter(`Series`=="Gdpe") 
+
+EXP_GDPE_ARIMA_MSE <- EXP_GDPE_PointF_arima %>% dplyr::select(`F-method`, `R-method`, `Forecast Horizon`, `MSE` ) %>% 
+  spread(key = `Forecast Horizon`, value = `MSE`) %>% 
+  mutate(`R-method` = replace(`R-method`, 6, "Naive")) %>%
+  dplyr::select(-`F-method`) %>%  
+  rename("Method" = "R-method") %>%
+  slice(match(Method_Order, `Method`)) %>% as.data.frame()
+
+EXP_GDPE_ARIMA_MSE %>% gather(-`Method`, key = "h", value = "MSE") -> EXP_GDPE_ARIMA_MSE
+
+EXP_GDPE_ARIMA_MSE %>% ggplot(aes(x = h, y = MSE, color = Method, shape = Method)) +
+  geom_point(size = 3) + 
+  scale_color_manual(values = c("green3", "brown")) +
+  scale_shape_manual(values=0:1) + ylab("MSE ") + ggtitle("Top level") +
+  theme(plot.title = element_text(size = 10, face = "italic")) -> Plot_EXP_PointF_GDPE_MSE
+
+#MASE
+EXP_GDPE_ARIMA_MASE <- EXP_GDPE_PointF_arima %>% dplyr::select(`F-method`, `R-method`, `Forecast Horizon`, `MASE` ) %>% 
+  spread(key = `Forecast Horizon`, value = `MASE`) %>% 
+  mutate(`R-method` = replace(`R-method`, 6, "Naive")) %>%
+  dplyr::select(-`F-method`) %>%  
+  rename("Method" = "R-method") %>%
+  slice(match(Method_Order, `Method`)) %>% as.data.frame()
+
+EXP_GDPE_ARIMA_MASE %>% gather(-`Method`, key = "h", value = "MASE") -> EXP_GDPE_ARIMA_MASE
+
+EXP_GDPE_ARIMA_MASE %>% ggplot(aes(x = h, y = MASE, color = Method, shape = Method)) +
+  geom_point(size = 3) + 
+  scale_color_manual(values = c("green3", "brown")) +
+  scale_shape_manual(values=0:1) + ylab("MASE ") + ggtitle("Top level") +
+  theme(plot.title = element_text(size = 10, face = "italic")) -> Plot_EXP_PointF_GDPE_MASE
+
+
+##All levels
+
+Score_arima %>% dplyr::select(-`Series`) %>% 
+  group_by(`F-method`, `R-method`, `Forecast Horizon`) %>%
+  summarise(Avg_MSE = round(mean(`MSE`), digits = 2), Avg_MASE = round(mean(`MASE`), digits = 2)) -> EXP_Score_all.series_ARIMA
+
+#MSE
+EXP_Score_all.series_ARIMA %>% dplyr::select(-`Avg_MASE`) %>% 
+  spread(key = `Forecast Horizon`, value = Avg_MSE) %>% 
+  ungroup() %>% dplyr::select(-`F-method`) %>%  
+  mutate(`R-method` = replace(`R-method`, 6, "Naive")) %>% rename("Method" = "R-method") %>%
+  slice(match(Method_Order, `Method`)) %>% as.data.frame() -> EXP_All.series.ARIMA_MSE
+
+EXP_All.series.ARIMA_MSE %>% gather(-`Method`, key = "h", value = "MSE") -> EXP_All.series.ARIMA_MSE
+
+EXP_All.series.ARIMA_MSE %>% ggplot(aes(x = h, y = MSE, color = Method, shape = Method)) +
+  geom_point(size = 3) + 
+  scale_color_manual(values = c("green3", "brown")) +
+  scale_shape_manual(values=0:1) + ylab("MSE ") + ggtitle("All levels") +
+  theme(plot.title = element_text(size = 10, face = "italic")) -> Plot_EXP_PointF_All.series_MSE
+
+#MASE
+EXP_Score_all.series_ARIMA %>% dplyr::select(-`Avg_MSE`) %>% 
+  spread(key = `Forecast Horizon`, value = Avg_MASE) %>% 
+  ungroup() %>% dplyr::select(-`F-method`) %>%  
+  mutate(`R-method` = replace(`R-method`, 6, "Naive")) %>% rename("Method" = "R-method") %>%
+  slice(match(Method_Order, `Method`)) %>% as.data.frame() -> EXP_All.series.ARIMA_MASE
+
+EXP_All.series.ARIMA_MASE %>% gather(-`Method`, key = "h", value = "MASE") -> EXP_All.series.ARIMA_MASE
+
+EXP_All.series.ARIMA_MASE %>% ggplot(aes(x = h, y = MASE, color = Method, shape = Method)) +
+  geom_point(size = 3) + 
+  scale_color_manual(values = c("green3", "brown")) +
+  scale_shape_manual(values=0:1) + ylab("MASE ") + ggtitle("All levels") +
+  theme(plot.title = element_text(size = 10, face = "italic")) -> Plot_EXP_PointF_All.series_MASE
+
+
+
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)}
+
+mylegend_MSE <- g_legend(Plot_INC_PointF_GDPI_MSE)
+
+grid.arrange( arrangeGrob(Plot_INC_PointF_All.series_MSE + theme(legend.position="none"), 
+                          Plot_INC_PointF_GDPI_MSE + theme(legend.position="none"), top="Income"), 
+              arrangeGrob(Plot_EXP_PointF_All.series_MSE + theme(legend.position="none"), 
+                          Plot_EXP_PointF_GDPE_MSE + theme(legend.position="none"), top="Expenditure"), 
+              ncol=2, mylegend_MSE, heights=c(10, 1))
+
+mylegend_MASE <- g_legend(Plot_INC_PointF_GDPI_MASE)
+
+grid.arrange( arrangeGrob(Plot_INC_PointF_All.series_MASE + theme(legend.position="none"), 
+                          Plot_INC_PointF_GDPI_MASE + theme(legend.position="none"), top="Income"), 
+              arrangeGrob(Plot_EXP_PointF_All.series_MASE + theme(legend.position="none"), 
+                          Plot_EXP_PointF_GDPE_MASE + theme(legend.position="none"), top="Expenditure"), 
+              ncol=2, mylegend_MASE, heights=c(10, 1))
